2f3b88cbda60389fa7c3fdd74d059f1a8eb44f74
Jem Zubiri <jemmybeaaar@gmail.com>
2018-03-19 06:56:12 +0800

    Added roles and new login modal pane for employees

--- session.php
+++ session.php
@@ -11,11 +11,12 @@ $user_check=$_SESSION['login_user'];
 
 // SQL Query To Fetch Complete Information Of User
 
-$ses_sql = mysqli_query($connection,"select username,id from usertbl where userEmail='$user_check'");
+$ses_sql = mysqli_query($connection,"select userName,id,userRole from usertbl where userEmail='$user_check'");
 $row = mysqli_fetch_assoc($ses_sql);
-$login_session = $row['username'];
+$userName = $row['userName'];
 $id=$row['id'];
-if(!isset($login_session)){
+$userRole = $row['userRole'];
+if(!isset($userName)){
 	mysql_close($connection); // Closing Connection
 	header('Location: index.php'); // Redirecting To Home Page
 }
