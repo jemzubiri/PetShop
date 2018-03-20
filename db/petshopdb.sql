589dd1058e1e6507dc2274504dfb1b1f8cd92a54
Jem Zubiri <jemmybeaaar@gmail.com>
2018-03-20 09:24:24 +0800

    new db 3/20/2018

--- db/petshopdb.sql
+++ db/petshopdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 19, 2018 at 12:20 PM
+-- Generation Time: Mar 20, 2018 at 02:23 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -34,16 +34,29 @@ CREATE TABLE `groomingtbl` (
   `groomPetGender` varchar(10) NOT NULL,
   `groomPetSize` varchar(10) NOT NULL,
   `groomList` varchar(200) NOT NULL,
-  `userId` varchar(11) NOT NULL
+  `userId` varchar(11) NOT NULL,
+  `referenceNo` varchar(40) NOT NULL,
+  `isActive` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `groomingtbl`
 --
 
-INSERT INTO `groomingtbl` (`groomId`, `groomBranch`, `groomDate`, `groomPetAge`, `groomPetGender`, `groomPetSize`, `groomList`, `userId`) VALUES
-(36, 'Robisons Town Mall Los Banos', '2018-03-29', 3, 'Female', 'Medium', 'Grooming Package (Bath, shampoo, blowdry, ear cleaning, nail trim, sanitary trim, teeth brushing, haircut)for small/medium - large/giant breeds,Ear Cleaning,', '32'),
-(37, 'Robisons Town Mall Los Banos', '2018-03-02', 33, '', 'Medium', 'Grooming Package (Bath, shampoo, blowdry, ear cleaning, nail trim, sanitary trim, teeth brushing, haircut)for small/medium - large/giant breeds,Ear Cleaning,', '');
+INSERT INTO `groomingtbl` (`groomId`, `groomBranch`, `groomDate`, `groomPetAge`, `groomPetGender`, `groomPetSize`, `groomList`, `userId`, `referenceNo`, `isActive`) VALUES
+(36, 'Robisons Town Mall Los Banos', '2018-03-29', 3, 'Female', 'Medium', 'Grooming Package (Bath, shampoo, blowdry, ear cleaning, nail trim, sanitary trim, teeth brushing, haircut)for small/medium - large/giant breeds,Ear Cleaning,', '32', 'ASD123', 'no'),
+(37, 'Robisons Town Mall Los Banos', '2018-03-02', 33, '', 'Medium', 'Grooming Package (Bath, shampoo, blowdry, ear cleaning, nail trim, sanitary trim, teeth brushing, haircut)for small/medium - large/giant breeds,Ear Cleaning,', '', 'ASD123', 'no'),
+(38, 'Robisons Town Mall Los Banos', '2018-03-21', 11, 'Male', 'Medium', 'Pet Masssage', '32', 'ASD123', 'yes'),
+(39, 'Robisons Town Mall Los Banos', '2018-03-22', 22, 'Male', 'Medium', 'Peticure Nail Caps Removal', '32', 'ASD123', 'yes'),
+(40, 'Robisons Town Mall Los Banos', '2018-03-02', 11, 'Female', 'Medium', 'Grooming Package (Bath, shampoo, blowdry, ear cleaning, nail trim, sanitary trim, teeth brushing, haircut)for small/medium - large/giant breeds', '32', 'ASD123', 'no'),
+(41, 'Robisons Town Mall Los Banos', '2018-03-20', 1, 'Female', 'Medium', 'Ear Cleaning,', '32', 'ASD123', 'yes'),
+(42, 'Robisons Town Mall Los Banos', '2018-03-22', 11, 'Female', 'Medium', 'Bath and Blowdry (for small/medium - large/giant breeds),', '32', 'ASD123', 'yes'),
+(43, 'Robisons Town Mall Los Banos', '2018-03-23', 11, 'Female', 'Medium', 'Grooming Package (Bath, shampoo, blowdry, ear cleaning, nail trim, sanitary trim, teeth brushing, haircut)for small/medium - large/giant breeds,Ear Cleaning,', '32', 'ASD123', 'no'),
+(44, 'Sunstar Mall', '2018-03-28', 11, 'Female', 'Medium', 'Grooming Package (Bath, shampoo, blowdry, ear cleaning, nail trim, sanitary trim, teeth brushing, haircut)for small/medium - large/giant breeds,', '32', 'ASD123', 'no'),
+(45, 'Sunstar Mall', '2018-03-28', 11, 'Female', 'Medium', 'Grooming Package (Bath, shampoo, blowdry, ear cleaning, nail trim, sanitary trim, teeth brushing, haircut)for small/medium - large/giant breeds,', '32', 'ASD123', 'no'),
+(46, 'Robisons Town Mall Los Banos', '2018-03-23', 33, 'Female', 'Medium', 'Ear Cleaning,Bath and Blowdry (for small/medium - large/giant breeds),', '32', '_3vqqjuo1u', 'no'),
+(47, 'Robisons Town Mall Los Banos', '2018-03-22', 3, 'Female', 'Medium', 'Grooming Package (Bath, shampoo, blowdry, ear cleaning, nail trim, sanitary trim, teeth brushing, haircut)for small/medium - large/giant breeds,', '32', '_qqmz50r7i', ''),
+(48, 'Robisons Town Mall Los Banos', '2018-03-23', 33, 'Female', 'Small', 'Ear Cleaning,Bath and Blowdry (for small/medium - large/giant breeds),', '32', '_g122dfbbl', '');
 
 -- --------------------------------------------------------
 
@@ -59,15 +72,34 @@ CREATE TABLE `servicestbl` (
   `servePetGender` varchar(10) NOT NULL,
   `servePetSize` varchar(10) NOT NULL,
   `serveList` varchar(200) NOT NULL,
-  `userId` int(11) NOT NULL
+  `userId` int(11) NOT NULL,
+  `referenceNo` varchar(40) NOT NULL,
+  `isActive` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `servicestbl`
 --
 
-INSERT INTO `servicestbl` (`serveId`, `serveBranch`, `serveDate`, `servePetAge`, `servePetGender`, `servePetSize`, `serveList`, `userId`) VALUES
-(81, 'Robisons Town Mall Los Banos', '2018-03-08', 2, 'Male', 'Medium', 'Dentistry,Surgeries', 32);
+INSERT INTO `servicestbl` (`serveId`, `serveBranch`, `serveDate`, `servePetAge`, `servePetGender`, `servePetSize`, `serveList`, `userId`, `referenceNo`, `isActive`) VALUES
+(81, 'Robisons Town Mall Los Banos', '2018-03-08', 2, 'Male', 'Medium', 'Dentistry,Surgeries', 32, '', 'no'),
+(82, 'Robisons Town Mall Los Banos', '2018-03-14', 11, 'Female', 'Medium', '', 32, '', 'yes'),
+(83, 'Robisons Town Mall Los Banos', '2018-03-22', 11, 'Female', 'Large', '', 32, '', 'yes'),
+(84, 'Robisons Town Mall Los Banos', '2018-03-28', 1, 'Male', 'Medium', '', 32, '', 'yes'),
+(85, 'Sunstar Mall', '2018-03-24', 22, 'Female', 'Medium', '', 32, '', 'yes'),
+(86, 'Robisons Town Mall Los Banos', '2018-03-30', 22, 'Female', 'Medium', '', 32, '', 'no'),
+(87, 'Robisons Town Mall Los Banos', '2018-03-22', 33, 'Male', 'Medium', 'Dentistry,Surgeries', 32, '', 'no'),
+(88, 'SM City Calamba', '2018-03-27', 22, 'Female', 'Medium', 'Dentistry,Surgeries', 32, '', 'no'),
+(89, 'Robisons Town Mall Los Banos', '2018-03-22', 33, 'Female', 'Medium', 'Dentistry,Surgeries', 32, '', 'no'),
+(90, 'Robisons Town Mall Los Banos', '2018-03-28', 1, 'Male', 'Medium', 'Dentistry,Surgeries', 32, '', 'no'),
+(91, 'Robisons Town Mall Los Banos', '2018-03-22', 2, 'Female', 'Medium', 'Prescription medication and diets', 32, '', 'no'),
+(92, 'Robisons Town Mall Los Banos', '2018-03-23', 11, 'Female', 'Medium', 'Dentistry,Surgeries', 32, '', 'no'),
+(93, 'Robisons Town Mall Los Banos', '2018-03-22', 2, 'Female', 'Medium', 'Deworming', 32, 'ASDASD', 'no'),
+(94, 'SM City Calamba', '2018-03-22', 2, 'Female', 'Medium', 'Dentistry', 32, 'Dentistry', 'no'),
+(95, 'Robisons Town Mall Los Banos', '2018-03-22', 1, 'Female', 'Medium', 'Surgeries', 32, 'Surgeries', 'no'),
+(96, 'Robisons Town Mall Los Banos', '2018-03-22', 23, 'Female', 'Medium', 'Prescription medication and diets', 32, '_xwimapltz', 'no'),
+(97, 'Robisons Town Mall Los Banos', '2018-03-22', 22, 'Male', 'Medium', 'Dentistry,Surgeries', 32, '_gefyp9ijt', 'no'),
+(98, 'SM City Calamba', '2018-03-22', -6, 'Male', 'Medium', 'Dentistry,Surgeries', 32, '_4wkffb5xs', '');
 
 -- --------------------------------------------------------
 
@@ -136,12 +168,12 @@ ALTER TABLE `usertbl`
 -- AUTO_INCREMENT for table `groomingtbl`
 --
 ALTER TABLE `groomingtbl`
-  MODIFY `groomId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `groomId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 --
 -- AUTO_INCREMENT for table `servicestbl`
 --
 ALTER TABLE `servicestbl`
-  MODIFY `serveId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `serveId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
 --
 -- AUTO_INCREMENT for table `usertbl`
 --
